07a761e9544bdc2a04949b8831880fa08cf71983
FairaIchsa <zelov02@yandex.ru>
2022-07-08 14:01:25 +0300

    обновил модель ресурсов

--- mainapp/models/resource_models.py
+++ mainapp/models/resource_models.py
@@ -3,11 +3,14 @@ from django.db import models
 
 class Resource(models.Model):
     MATERIAL_TYPES = (
-        ('GS', 'Gas'),  # ?
-        ('LQ', 'Liquid'),
-        ('CT', 'Container'),  # нужно прикрутить механику для контейнеров разных размеров
-        ('LS', 'Loose')
+        ('GS', 'Gas'),  # 1000 кубических метров
+        ('LQ', 'Liquid'),  # ?
+        ('CT', 'Container'),  # 1 контейнер
+        ('LS', 'Loose')  # 500 тонн
     )
 
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=255, choices=MATERIAL_TYPES)
+
+    def __str__(self):
+        return f"{self.name} ({self.get_type_display()})"
