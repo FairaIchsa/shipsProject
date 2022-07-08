398fc51db79da8cc50deaaa6e8b4459ed0280727
FairaIchsa <zelov02@yandex.ru>
2022-07-08 14:58:28 +0300

    обновил модель грузов

--- mainapp/models/__init__.py
+++ mainapp/models/__init__.py
@@ -1,1 +1,2 @@
-from mainapp.models import cargo_models, edge_models, node_models, resource_models, ship_models
+from mainapp.models import cargo_models, edge_models, node_models, resource_models, ship_models, \
+                           order_models
--- mainapp/models/cargo_models.py
+++ mainapp/models/cargo_models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 from mainapp.models.ship_models import TransportShip
+from mainapp.models.resource_models import Resource
 
 
 class Cargo(models.Model):
-    owner = models.ForeignKey(TransportShip, on_delete=models.PROTECT)
+    owner = models.ForeignKey(TransportShip, on_delete=models.PROTECT, related_name='cargos')
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
 
+    def __str__(self):
+        return f"{self.id} Cargo {self.resource.name} of {self.owner}"
