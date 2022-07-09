1ab2d8f8dcbd3826e2690a89fa428b3a23761942
FairaIchsa <zelov02@yandex.ru>
2022-07-09 22:04:08 +0300

    обновил модель кораблей

--- mainapp/models/ship_models.py
+++ mainapp/models/ship_models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from mainapp.models.node_models import Node
+from mainapp.models.edge_models import SeaRoute
+
 
 class TransportShip(models.Model):
     SHIP_TYPES = (
@@ -9,3 +12,13 @@ class TransportShip(models.Model):
 
     type = models.CharField(max_length=255, choices=SHIP_TYPES)
     name = models.CharField(max_length=255)
+    speed = models.DecimalField(max_digits=6, decimal_places=2)
+    arc_type = models.PositiveSmallIntegerField()
+    load_capacity = models.PositiveIntegerField()
+    current_load = models.PositiveIntegerField()
+
+    class Navigation:
+        sea_port = models.ForeignKey(Node, null=True, related_name='ships', on_delete=models.CASCADE)
+        sea_route = models.ForeignKey(SeaRoute, null=True, related_name='ships', on_delete=models.CASCADE)
+        departure = models.DateTimeField(null=True)
+        arrival = models.DateTimeField(null=True)
