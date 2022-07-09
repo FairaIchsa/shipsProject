8973254a2faa371243eb8c7671fd7ca836f6b2b6
FairaIchsa <zelov02@yandex.ru>
2022-07-09 16:35:07 +0300

    обновил модель узлов

--- mainapp/models/node_models.py
+++ mainapp/models/node_models.py
@@ -2,17 +2,16 @@ from django.db import models
 
 
 class Node(models.Model):
+    NODE_TYPES = (
+        ('SP', 'Seaport'),
+        ('L', 'Land')
+    )
+
     name = models.CharField(max_length=255)
+    type = models.CharField(max_length=255, choices=NODE_TYPES)
 
     coord_x = models.DecimalField(max_digits=5, decimal_places=2)
     coord_y = models.DecimalField(max_digits=5, decimal_places=2)
 
-    available_cargo = None
-    requested_cargo = None
-    contracted_cargo = None
-
-    def deliver_from(self):
-        pass
-
-    def deliver_to(self):
-        pass
+    def __str__(self):
+        return f"{self.name} ({self.get_type_display()})"
