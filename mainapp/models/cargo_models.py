7eb388a5efb43c77ed4482d0409a01f6c5d2e980
FairaIchsa <zelov02@yandex.ru>
2022-07-09 21:32:38 +0300

    добавил модель для наземных грузов

--- mainapp/models/cargo_models.py
+++ mainapp/models/cargo_models.py
@@ -2,12 +2,28 @@ from django.db import models
 
 from mainapp.models.ship_models import TransportShip
 from mainapp.models.resource_models import Resource
+from mainapp.models.edge_models import LandRoute
 
 
 class Cargo(models.Model):
-    owner = models.ForeignKey(TransportShip, on_delete=models.PROTECT, related_name='cargos')
+    class Meta:
+        abstract = True
+
     resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
 
+
+class ShipCargo(Cargo):
+    owner = models.ForeignKey(TransportShip, on_delete=models.PROTECT, related_name='cargos')
+
     def __str__(self):
         return f"{self.id} Cargo {self.resource.name} of {self.owner}"
+
+
+class LandCargo(models.Model):
+    route = models.ForeignKey(LandRoute, on_delete=models.PROTECT, related_name='cargos')
+    departure = models.DateTimeField()  # возможно, стоит сделать PositiveBigIntegerField
+    arrival = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.id} Cargo {self.resource.name} on {self.route}"
