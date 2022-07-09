aad35dd4141a286b2ba189886dd11822b509f92c
FairaIchsa <zelov02@yandex.ru>
2022-07-09 21:23:56 +0300

    обновил модель морских и наземных путей

--- mainapp/models/edge_models.py
+++ mainapp/models/edge_models.py
@@ -7,34 +7,34 @@ class Edge(models.Model):
     class Meta:
         abstract = True
 
-    node_0 = None
-    node_1 = None
+    start_node = models.ForeignKey(Node, on_delete=models.PROTECT, related_name='start_for')
+    end_node = models.ForeignKey(Node, on_delete=models.PROTECT, related_name='end_for')
     length = models.DecimalField(max_digits=10, decimal_places=2)
 
 
-class SeaRoute(Edge):
-    node_0 = models.ForeignKey(Node, on_delete=models.PROTECT, related_name='sea_start_for')
-    node_1 = models.ForeignKey(Node, on_delete=models.PROTECT, related_name='sea_end_for')
+class SeaRoute(Edge):  # добавить валидатор: оба нода должны быть морскими
+    start_node = models.ForeignKey(Node, on_delete=models.PROTECT, related_name='sea_start_for')
+    end_node = models.ForeignKey(Node, on_delete=models.PROTECT, related_name='sea_end_for')
     ice_level = models.PositiveSmallIntegerField()  # добавить validator
     fullness = models.PositiveSmallIntegerField()   # текущая заполненность
     bandwidth = models.PositiveSmallIntegerField()  # пропускная способность
 
     def __str__(self):
-        return f"Sea route {self.id}"
+        return f"Sea route {self.start_node}-{self.end_node}"
 
 
-class Supply(Edge):
+class LandRoute(Edge):
     TYPE_CHOICES = (
         ('RW', 'Railway'),
         ('PL', 'Pipeline')
     )
 
-    node_0 = models.ForeignKey(Node, on_delete=models.PROTECT, related_name='land_start_for')
-    node_1 = models.ForeignKey(Node, on_delete=models.PROTECT, related_name='land_end_for')
+    start_node = models.ForeignKey(Node, on_delete=models.PROTECT, related_name='land_start_for')
+    end_node = models.ForeignKey(Node, on_delete=models.PROTECT, related_name='land_end_for')
     type = models.CharField(max_length=255, choices=TYPE_CHOICES)
 
     def __str__(self):
-        return f"{self.type} {self.id}"
+        return f"{self.type} {self.start_node}-{self.end_node}"
 
 
 
